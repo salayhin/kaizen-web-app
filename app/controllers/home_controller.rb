a8a00d01e0842a3d734acec267aa0abcd05dd44d
salayhin <salayhin@gmail.com>
2015-12-14 04:13:34 +0600

    Map added

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
   def index
+    @weathers = Weather.all
+    @hash = Gmaps4rails.build_markers(@weathers) do |weather, marker|
+      marker.lat weather.lat
+      marker.lng weather.long
+    end
   end
 end
--- app/controllers/weathers_controller.rb
+++ app/controllers/weathers_controller.rb
@@ -7,6 +7,10 @@ class WeathersController < ApplicationController
   # GET /weathers.json
   def index
     @weathers = Weather.all
+    @hash = Gmaps4rails.build_markers(@weathers) do |weather, marker|
+      marker.lat weather.lat
+      marker.lng weather.long
+    end
   end
 
   # GET /weathers/1
